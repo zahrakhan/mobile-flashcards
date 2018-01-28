d8f76ac4abcf616eb7847e91356f61822095811d
ZahraKhan <zahra.khan@arbisoft.com>
2018-01-28 20:17:04 +0500

    feat: store dispath logger added

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import {StyleSheet, Text, View} from 'react-native'
-import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducers'
 
 import AddDeck from './components/AddDeck'
+import {logger} from './utils/logger'
+
+const middleware = applyMiddleware(logger)
+let store = createStore(reducer, middleware)
+
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider store={store}>
         <View style={styles.container}>
           <AddDeck/>
         </View>
