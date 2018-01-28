e04bbb6989b8b272eb41743df8d3b4e47d822d90
ZahraKhan <zahra.khan@arbisoft.com>
2018-01-28 20:39:22 +0500

    feat: Add customized status bar

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducers'
 
 import AddDeck from './components/AddDeck'
+import FlashcardsStatusBar from './components/FlashcardsStatusBar'
+import {teal} from './utils/colors'
 import {logger} from './utils/logger'
 
 const middleware = applyMiddleware(logger)
@@ -15,6 +17,7 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
         <View style={styles.container}>
+          <FlashcardsStatusBar backgroundColor={teal} barStyle="light-content"/>
           <AddDeck/>
         </View>
       </Provider>
