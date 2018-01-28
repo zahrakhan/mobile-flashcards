36bc188eedc2e589915f728030cbbf91cc81a2b8
ZahraKhan <zahra.khan@arbisoft.com>
2018-01-28 22:26:53 +0500

    feat: Add Decks component to list all decks

---
+++ components/Decks.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react'
+import {Text, View, FlatList, StyleSheet, Platform} from 'react-native'
+import {connect} from 'react-redux'
+
+import {receiveDecks} from '../actions'
+import {getDecks} from '../utils/api'
+import {white, cyan_dark, gray, gray_lighter} from '../utils/colors'
+
+class Decks extends Component {
+    componentDidMount() {
+        const {dispatch} = this.props
+
+        getDecks().then((decks) => dispatch(receiveDecks(decks)))
+    }
+
+    keyExtractor = (item, index) => item.title
+    renderItem = ({item}) => {
+        const {title, questions} = item
+        return (
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>{title}</Text>
+                <Text style={styles.itemDetail}>{`${questions.length? questions.length: `No`} cards`}</Text>
+            </View>
+        )
+    }
+
+    render() {
+        const {decks} = this.props
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={Object.values(decks)}
+                    keyExtractor={this.keyExtractor}
+                    renderItem={this.renderItem}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: gray_lighter
+    },
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios'
+            ? 16
+            : 2,
+        padding: 30,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        }
+    },
+    itemTitle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: cyan_dark,
+        paddingBottom: 10,
+    },
+    itemDetail: {
+        fontSize: 16,
+        color: gray,
+
+    },
+    noDataText: {
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20
+    }
+})
+
+function mapStateToProps(decks) {
+    return {decks}
+}
+
+export default connect(mapStateToProps,)(Decks)
--- utils/colors.js
+++ utils/colors.js
@@ -1,10 +1,11 @@
 // utils/colors.js
 
 export const teal = '#008080'
-export const cyan = '#00FFFF'
+export const cyan = '#007777'
 export const cyan_dark = '#008B8B'
 export const gray = '#757575'
-export const gray_light = '#c7c7c7'
+export const gray_light = '#edeeed'
+export const gray_lighter = '#f5f6f5' //F5F6F5'//'#edeeed'//'#c7c7c7'
 export const white = '#fff'
 export const red = '#b71845'
 export const green = '#006400'
