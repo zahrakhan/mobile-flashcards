bd62077e7793ec73a8d64a0f6bce78bcfeac5999
ZahraKhan <zahra.khan@arbisoft.com>
2018-01-31 23:25:43 +0500

    feat: Animate card flip on quiz

---
+++ components/Card.js
@@ -0,0 +1,200 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Animated,
+    Platform
+} from 'react-native';
+import {Ionicons} from '@expo/vector-icons'
+
+import TextButton from './TextButton'
+import {white, gray_lighter, red, gray} from '../utils/colors'
+
+EyeIcon = () => (<Ionicons
+    name={Platform === 'ios'
+    ? 'ios-eye'
+    : 'md-eye'}
+    size={15}
+    color={red}/>)
+
+CardFlipButton = ({label, onPress}) => (
+    <TextButton
+        style={{
+        color: red,
+        fontSize: 16
+    }}
+        onPress={onPress}>
+        <EyeIcon/> {label}
+    </TextButton>
+)
+
+export default class Card extends Component {
+    state = {
+        isFlipped: false
+    }
+    constructor(props) {
+        super(props)
+    }
+    componentWillMount() {
+        this.configureFlipAnimation()
+    }
+    // flip card back if answer is showing
+    componentWillReceiveProps() {
+        if (this.state.isFlipped) {
+            this.flipCard()
+        }
+    }
+    configureFlipAnimation = () => {
+        this.animatedValue = new Animated.Value(0);
+        this.value = 0;
+        this
+            .animatedValue
+            .addListener(({value}) => {
+                this.value = value;
+            })
+        this.frontInterpolate = this
+            .animatedValue
+            .interpolate({
+                inputRange: [
+                    0, 180
+                ],
+                outputRange: ['0deg', '180deg']
+            })
+        this.backInterpolate = this
+            .animatedValue
+            .interpolate({
+                inputRange: [
+                    0, 180
+                ],
+                outputRange: ['180deg', '360deg']
+            })
+    }
+    flipCard = () => {
+        this.setState((state) => ({
+            isFlipped: !state.isFlipped
+        }))
+        if (this.value >= 90) {
+            Animated
+                .spring(this.animatedValue, {
+                toValue: 0,
+                friction: 8,
+                tension: 10
+            })
+                .start();
+        } else {
+            Animated
+                .spring(this.animatedValue, {
+                toValue: 180,
+                friction: 8,
+                tension: 10
+            })
+                .start();
+        }
+    }
+
+    render() {
+        const frontAnimatedStyle = {
+            transform: [
+                {
+                    rotateY: this.frontInterpolate
+                }
+            ]
+        }
+        const backAnimatedStyle = {
+            transform: [
+                {
+                    rotateY: this.backInterpolate
+                }
+            ]
+        }
+        const {cardNumber, totalCards, question, answer} = this.props
+        const counter = `${cardNumber}/${totalCards}`
+        return (
+
+            <View style={styles.container}>
+                <View style={{
+                    flex: 1
+                }}>
+                    <Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
+                        <Text style={styles.flipCardCounter}>
+                            {counter}
+                            <Text style={styles.flipCardTitle}>
+                                {`  Question`}
+                            </Text>
+                        </Text>
+
+                        <Text style={styles.flipCardText}>
+                            {question}
+                        </Text>
+                        <CardFlipButton label='Answer' onPress={this.flipCard}/>
+                    </Animated.View>
+                    <Animated.View
+                        style={[backAnimatedStyle, styles.flipCard, styles.flipCardBack]}>
+                        <Text style={styles.flipCardCounter}>
+                            {counter}
+                            <Text style={styles.flipCardTitle}>
+                                {`  Answer`}
+                            </Text>
+                        </Text>
+                        <Text style={styles.flipCardText}>
+                            {answer}
+                        </Text>
+                        <CardFlipButton label='Question' onPress={this.flipCard}/>
+                    </Animated.View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: gray_lighter,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    flipCard: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios'
+            ? 16
+            : 2,
+        padding: 30,
+        height: 250,
+        width: 300,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        backfaceVisibility: 'hidden'
+    },
+    flipCardBack: {
+        backgroundColor: white,
+        position: "absolute",
+        top: 0
+    },
+    flipCardTitle: {
+        color: gray,
+        fontSize: 16,
+        fontWeight: 'normal'
+    },
+    flipCardCounter: {
+        fontWeight: 'bold'
+    },
+    flipCardText: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingBottom: 10
+    }
+});
--- components/Quiz.js
+++ components/Quiz.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 import {Text, View, StyleSheet, Platform} from 'react-native'
 import {connect} from 'react-redux'
-import {FontAweome, Ionicons} from '@expo/vector-icons'
+import {Ionicons} from '@expo/vector-icons'
 
 import SubmitButton from './SubmitButton'
 import TextButton from './TextButton'
+import Card from './Card'
 import {
     white,
     cyan_dark,
@@ -14,13 +15,6 @@ import {
     red
 } from '../utils/colors'
 import {clearLocalNotification, setLocalNotification} from '../utils/notification'
-
-EyeIcon = () => (<Ionicons
-    name={Platform === 'ios'
-    ? 'ios-eye'
-    : 'md-eye'}
-    size={15}
-    color={red}/>)
 
 RestartIcon = () => (<Ionicons
     name={Platform === 'ios'
@@ -32,26 +26,22 @@ RestartIcon = () => (<Ionicons
 class Quiz extends Component {
     state = {
         score: 0,
-        counter: 0,
-        showAnswer: false
+        counter: 0
     }
     handleCorrectAnswer = () => {
-        const {score, counter, showAnswer} = this.state
-        this.updateQuizState(score + 1, counter + 1, false)
+        const {score, counter} = this.state
+        this.updateQuizState(score + 1, counter + 1)
     }
 
     handleIncorrectAnswer = () => {
-        const {score, counter, showAnswer} = this.state
-        this.updateQuizState(score, counter + 1, false)
+        const {score, counter} = this.state
+        this.updateQuizState(score, counter + 1)
     }
-    updateQuizState = (score = 0, counter = 0, showAnswer = false) => {
-        this.setState({score, counter, showAnswer})
+
+    updateQuizState = (score = 0, counter = 0) => {
+        this.setState({score, counter})
     }
-    flipCard = () => {
-        this.setState((state) => ({
-            showAnswer: !state.showAnswer
-        }))
-    }
+
     getScoreMessage = (score) => {
         const exclamation = score < 35
             ? 'Opps'
@@ -68,28 +58,14 @@ class Quiz extends Component {
     }
     render() {
         const {title, questions} = this.props.deck
-        const {showAnswer, counter, score} = this.state
+        const {counter, score} = this.state
         const card = questions[counter]
         const total_cards = questions.length
         if (counter < total_cards) {
             return (
                 <View style={styles.container}>
-                    <View style={[styles.item, styles.quizCard]}>
-                        <Text style={styles.quizCounter}>{`${counter + 1}/${total_cards}`}</Text>
-                        <Text style={styles.itemTitle}>{showAnswer
-                                ? card.answer
-                                : card.question}</Text>
-
-                        <TextButton
-                            style={{
-                            color: red,
-                            fontSize: 16
-                        }}
-                            onPress={this.flipCard}>
-                            <EyeIcon/> {showAnswer
-                                ? ` Question`
-                                : ` Answer`}
-                        </TextButton>
+                    <View style={styles.card_container}>
+                        <Card cardNumber={counter + 1} totalCards={total_cards} {...card}/>
                     </View>
                     <View style={[styles.actions, styles.center]}>
                         <SubmitButton
@@ -109,12 +85,11 @@ class Quiz extends Component {
             const result = ((score / total_cards) * 100).toFixed(2)
             this.resetDailyQuizNotification()
             return (
-                <View style={[styles.container, styles.backgroundColor]}>
+                <View style={[styles.container, styles.quizResult]}>
                     <View style={styles.center}>
-                        <Text style={styles.itemTitle}>{this.getScoreMessage(result)}</Text>
+                        <Text style={styles.quizResultTitle}>{this.getScoreMessage(result)}</Text>
                         <TextButton onPress={this.restartQuiz}>
-                            <RestartIcon/>
-                            {` Restart Quiz`}
+                            <RestartIcon/> {` Restart Quiz`}
                         </TextButton>
                     </View>
                 </View>
@@ -128,6 +103,9 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: gray_lighter
     },
+    card_container: {
+        flex: 1
+    },
     center: {
         flex: 1,
         justifyContent: 'center',
@@ -135,44 +113,14 @@ const styles = StyleSheet.create({
         marginLeft: 30,
         marginRight: 30
     },
-    item: {
-        backgroundColor: white,
-        borderRadius: Platform.OS === 'ios'
-            ? 16
-            : 2,
-        padding: 30,
-        marginLeft: 10,
-        marginRight: 10,
-        marginTop: 17,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowRadius: 3,
-        shadowOpacity: 0.8,
-        shadowColor: 'rgba(0, 0, 0, 0.24)',
-        shadowOffset: {
-            width: 0,
-            height: 3
-        }
-    },
-    quizCard: {
-        flex: 1,
-        justifyContent: 'space-between'
-    },
-    quizCounter: {
-        fontWeight: 'bold'
-    },
     quizResult: {
         backgroundColor: white
     },
-    itemTitle: {
+    quizResultTitle: {
         fontSize: 22,
         fontWeight: 'bold',
         textAlign: 'center',
         paddingBottom: 10
-    },
-    itemDetail: {
-        fontSize: 16,
-        color: gray
     },
     actions: {
         flex: 2
